6da0c4fee5a60f9ac8996eaed21f799b21df6071
Christoph Kopf <christoph.kopf@adliance.net>
2020-10-20 22:43:11 +0200

    switch propterty position in code

--- src/Adliance.AzureTools/DatabaseTools/Parameters/ImportDatabaseParameters.cs
+++ src/Adliance.AzureTools/DatabaseTools/Parameters/ImportDatabaseParameters.cs
@@ -8,8 +8,8 @@ namespace Adliance.AzureTools.DatabaseTools.Parameters
         [Option('s', "source", Required = true, Default = "", HelpText = "The source file path of the *.bacpac.")] public string Source { get; set; } = "";
         [Option('t', "target", Required = true, Default = "", HelpText = "The connection string to the target database.")] public string Target { get; set; } = "";
         
-        [Option( 'p',"elastic-pool", Required = false, Default = "", HelpText = "If set, the target database will be moved to the specified elastic pool.")] public string ElasticPool { get; set; } = "";
-
         [Option('f', "force", Required = false, Default = false, HelpText = "Force import-database operation and skip user confirmation or interaction")] public bool Force { get; set; } = false;
+        
+        [Option( 'p',"elastic-pool", Required = false, Default = "", HelpText = "If set, the target database will be moved to the specified elastic pool.")] public string ElasticPool { get; set; } = "";
     }
 }
