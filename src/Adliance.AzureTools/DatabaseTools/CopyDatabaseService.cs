2cc10c04a214206645681ddbc4bfb43cab37b64c
Christoph Kopf <christoph.kopf@adliance.net>
2020-10-20 22:34:40 +0200

    reuse existing export / import

--- src/Adliance.AzureTools/DatabaseTools/CopyDatabaseService.cs
+++ src/Adliance.AzureTools/DatabaseTools/CopyDatabaseService.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Data.SqlClient;
-using System.IO;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Adliance.AzureTools.DatabaseTools.Parameters;
 
@@ -19,84 +16,14 @@ namespace Adliance.AzureTools.DatabaseTools
         public async Task Run()
         {
             try
-            {
-                var sourceDbName = FindDatabaseName(_parameters.Source);
-                var targetDbName = FindDatabaseName(_parameters.Target);
-
-                var forceOperation = _parameters.Force;
-                if (forceOperation)
-                {
-                    Console.WriteLine("[Force] Skipping connection string check...");
-                }
-                else
-                {
-                    var azureDbStringResult = AnalyzeAzureDbString(_parameters.Target);
-                    
-                    // User should confirm operation, since we're going to manipulate an azure-hosted database
-                    if (azureDbStringResult.IsAzureConnectionString)
-                    {
-                        var confirmationResult = UserConfirmAzureDatabaseTarget(azureDbStringResult.AzureDbSubdomain);
-                        if (confirmationResult == AzureTargetConfirmation.AbortOperation)
-                        {
-                            Console.WriteLine("Exiting...");
-                            return;
-                        }                        
-                    }
-                }
+            {   var exportDatabaseParameters = ExportDatabaseParametersFromCopyDatabaseParameters(_parameters);
+                var exportDatabaseService = new ExportDatabaseService(exportDatabaseParameters);
+                var bacpacPath = await exportDatabaseService.Run();
                 
-                var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), $"{sourceDbName}.bacpac");
-                if (_parameters.UseLocalIfExists && File.Exists(fileName))
-                {
-                    Console.WriteLine($"Using local file \"{fileName}\".");
-                }
-                else
-                {
-                    Console.WriteLine($"Downloading database to \"{fileName}\" ...");
-                    SqlPackageAdapter.ExportDatabase(_parameters.Source, fileName);
-                }
+                var importDatabaseParameters = ImportDatabaseParametersFromCopyDatabaseParameters(_parameters, bacpacPath);
+                var importDatabaseService = new ImportDatabaseService(importDatabaseParameters);
+                await importDatabaseService.Run();
 
-                var temporaryDbName = targetDbName + "_" + Guid.NewGuid();
-                Console.WriteLine($"Restoring to temporary database \"{temporaryDbName}\" ...");
-                SqlPackageAdapter.ImportDatabase(_parameters.Target.Replace(targetDbName, temporaryDbName), fileName);
-
-                await using (var connection = new SqlConnection(_parameters.Target.Replace(targetDbName, "master")))
-                {
-                    await connection.OpenAsync();
-
-                    var databaseExists = await SqlScalar(connection, $"SELECT COUNT(*) from master.sys.databases where name='{targetDbName}';");
-                    if (databaseExists is int i && i > 0)
-                    {
-                        Console.WriteLine("Deleting existing database ...");
-                        try
-                        {
-                            await SqlCommand(connection, $"ALTER DATABASE [{targetDbName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;");
-                        }
-                        catch 
-                        {
-                           // do nothing here, fails in Azure but has no effect usually
-                        }
-
-                        try
-                        {
-                            await SqlCommand(connection, $"DROP DATABASE [{targetDbName}];");
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Unable to delete existing database \"{targetDbName}\": {ex.Message}");
-                        }
-                    }
-
-                    Console.WriteLine($"Renaming temporary database to \"{targetDbName}\" ...");
-                    await SqlCommand(connection, $"ALTER DATABASE [{temporaryDbName}] MODIFY NAME = [{targetDbName}];");
-
-                    if (!string.IsNullOrWhiteSpace(_parameters.ElasticPool))
-                    {
-                        Console.WriteLine($"Setting elastic pool to \"{_parameters.ElasticPool}\" ...");
-                        await SqlCommand(connection, $"ALTER DATABASE [{targetDbName}] MODIFY ( SERVICE_OBJECTIVE = ELASTIC_POOL ( name = [{_parameters.ElasticPool}] ) );");
-                    }
-                }
-
-                Console.WriteLine("Everything done.");
             }
             catch (Exception ex)
             {
@@ -104,81 +31,24 @@ namespace Adliance.AzureTools.DatabaseTools
             }
         }
 
-        private enum AzureTargetConfirmation
+        public ExportDatabaseParameters ExportDatabaseParametersFromCopyDatabaseParameters(CopyDatabaseParameters parameters)
         {
-            AbortOperation,
-            Confirmed
-        }
-        
-        private AzureTargetConfirmation UserConfirmAzureDatabaseTarget(string targetAzureDbUrl)
-        {
-            Console.WriteLine("[CRITICAL] The defined target is hosted at Microsoft Azure. This operation will overwrite the target with the defined source. Without a backup, all data could be lost forever.");
-
-            var maxRetries = 3;
-            for (var retries = maxRetries; retries > 0; --retries)
+            return new ExportDatabaseParameters
             {
-                Console.WriteLine($"Enter '{targetAzureDbUrl}' to confirm the operation, to abort leave empty and hit enter ({retries} retries left):");
-                    
-                var confirmationString = Console.ReadLine();
-                if (string.IsNullOrEmpty(confirmationString))
-                {
-                    Console.WriteLine("Aborting copy-database operation...");
-                    return AzureTargetConfirmation.AbortOperation;
-                }
-                
-                if (confirmationString == targetAzureDbUrl)
-                {
-                    Console.WriteLine("Confirmed, continue operation...");
-                    return AzureTargetConfirmation.Confirmed;
-                }
-            }
-            
-            Console.WriteLine("Too may retries. Aborting copy-database operation...");
-            return AzureTargetConfirmation.AbortOperation;
-        }
-        
-        private async Task SqlCommand(SqlConnection connection, string sql)
-        {
-            await using (var command = new SqlCommand(sql, connection))
-            {
-                await command.ExecuteNonQueryAsync();
-            }
-        }
-
-        private async Task<object> SqlScalar(SqlConnection connection, string sql)
-        {
-            await using (var command = new SqlCommand(sql, connection))
-            {
-                return await command.ExecuteScalarAsync();
-            }
-        }
-        
-        private string FindDatabaseName(string connectionString)
-        {
-            var match = Regex.Match(connectionString, @"[ ;]*Initial Catalog[ ]*\=(.*?)[;$]", RegexOptions.IgnoreCase);
-            if (match.Success)
-            {
-                return match.Groups[1].Value.Trim();
-            }
-
-            throw new Exception($"No database name found in \"{connectionString}\".");
-        }
-
-        private struct AzureConnectionStringResult
-        {
-            public bool IsAzureConnectionString { get; set; }
-            public string AzureDbSubdomain { get; set; }
-        }
-        
-        private AzureConnectionStringResult AnalyzeAzureDbString(string connectionString)
-        {
-            var match = Regex.Match(connectionString, @"[ ;]*Server[ ]*\=[ ]*tcp:(.*?)\.database\.windows\.net.*[;$]", RegexOptions.IgnoreCase);
-
-            return new AzureConnectionStringResult
-            {
-                IsAzureConnectionString = match.Success,
-                AzureDbSubdomain = match.Success && match.Groups.Count >= 2 ? match.Groups[1].Value.Trim() : "" 
+                Source = parameters.Source,
+                Force = !_parameters.UseLocalIfExists
             };
-        }
+        } 
+        
+        public ImportDatabaseParameters ImportDatabaseParametersFromCopyDatabaseParameters(CopyDatabaseParameters parameters, string bacpacPath)
+        {
+            return new ImportDatabaseParameters
+            { 
+                Source = bacpacPath,
+                Target = parameters.Target,
+                Force = parameters.Force,
+                ElasticPool = parameters.ElasticPool
+            };
+        } 
     }
 }
